a8357101501abca8546ec20faf2a1db470e1b3db
SparrowNado <123578310+SparrowNado@users.noreply.github.com>
2023-09-01 13:50:20 +0800

    Update page.tsx

--- convers8te-discord-clone/app/page.tsx
+++ convers8te-discord-clone/app/page.tsx
@@ -1,10 +1,16 @@
+import { Button } from "@/components/ui/button";
 
 
 export default function Home() {
   return (
 
+    <div className="flex flex-col">
     //text-3xl makes the text larger while font-bold makes it bold
 
    <p className='text-3xl font-bold text-indigo-500'>Hello World</p>
+   <Button variant="destructive" className="bg-indigo-500">
+    Press Here
+    </Button> 
+   </div>
   )
 }
