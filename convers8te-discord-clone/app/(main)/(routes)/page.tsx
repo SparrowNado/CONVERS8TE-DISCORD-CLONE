6fc456efed577fbe2a186828f71f7fa383f8d257
SparrowNado <123578310+SparrowNado@users.noreply.github.com>
2023-09-01 15:15:51 +0800

    Update page.tsx

--- convers8te-discord-clone/app/(main)/(routes)/page.tsx
+++ convers8te-discord-clone/app/(main)/(routes)/page.tsx
@@ -8,12 +8,10 @@ export default function Home() {
   return (
 
     <div className="flex flex-col">
-    //text-3xl makes the text larger while font-bold makes it bold
 
-   <p className='text-3xl font-bold text-indigo-500'>Hello World</p>
-   <Button variant="destructive" className="bg-indigo-500">
-    Press Here
-    </Button> 
+
+
+   Protected Route
 
    
    </div>
